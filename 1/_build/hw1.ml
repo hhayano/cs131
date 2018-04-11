e8119cd6a04598dcec4a2db89a9a54bcd2af4e66
Hajime Hayano <hajimehayano@gmail.com>
2018-04-10 23:09:44 -0700

    First commit, hw 1

---
+++ 1/_build/hw1.ml
@@ -0,0 +1,8 @@
+let print str =
+  print_endline str;;
+
+print "Hi";;
+print "My name is haji";;
+
+let x =
+  prompt_string "Enter your name:";;
---
+++ 1/hw1.ml
@@ -0,0 +1,9 @@
+let print str =
+  print_endline str;;
+
+print "Hi";;
+print "My name is haji";;
+
+(* Subset *)
+let rec subset s1 s2 =
+  match s1 
