cdf3147699fe782792a126a438dd3ed16368775e
Hajime Hayano <hajimehayano@gmail.com>
2018-04-15 20:24:07 -0700

    start on finding blind alley

--- 1/hw1.ml
+++ 1/hw1.ml
@@ -1,6 +1,11 @@
 open List
 open Printf
 
+type ('nonterminal, 'terminal) symbol =
+  | N of 'nonterminal
+  | T of 'terminal
+;;
+
 (* Subset *)
 let rec subset s1 s2 =
   match s1 with
@@ -81,6 +86,127 @@ let rec rle_decode lp =
   repeat i e @ rle_decode t
 ;;
 
+(* filter blind alley rules *)
+(*
+let filter_blind_alleys g =
+  let rec all_terminal rhs =
+    match rhs with
+    | [] -> true
+    | hd::tl ->
+      match hd with
+      | N _ -> false
+      | T _ -> all_terminal tl
+  in
+  match g with
+  | (start_symbol, rule_list) -> 
+    let rec function filter_rules_list =
+      [] -> []
+    match rule_list with
+    | [] -> (start_symbol, [])
+    | hd::tl -> 
+      match hd with
+      | (lhs, rhs) ->
+        match all_terminal rhs with
+        | true -> 
+
+let rec reduction_check rule rule_list =
+  match rule with (lhs, rhs) ->
+    match rhs with
+    | [] -> false
+    | 
+
+
+let rec function filter_rules_list =
+  | [] -> []
+  | hd::tl -> 
+    match hd with (lhs, rhs) -> 
+      match all_terminal rhs with
+      | true -> hd::(filter_rules_list tl)
+      | false ->
+        match reduction_check tl with
+        | true -> hd::(filter_rules_list tl)
+        | false -> filter_rules_list
+;;
+
+let rec all_terminal rhs =
+  match rhs with
+  | [] -> true
+  | hd::tl ->
+    match hd with
+    | N _ -> false
+    | T _ -> all_terminal tl
+;;
+*)
+
+let rec remove_empty_rhs rules_list =
+  match rules_list with
+  | [] -> []
+  | hd::tl ->
+    match hd with (lhs, rhs) ->
+      match rhs with
+      | [] -> remove_empty_rhs tl
+      | _ -> hd::(remove_empty_rhs tl)
+;;
+
+let rec remove_redundant_rules rules_list =
+  match rules_list with
+  | [] -> []
+  | hd::tl ->
+    match hd with (lhs, rhs) ->
+      match rhs with
+      | [e] ->
+        if e = lhs then remove_redundant_rules tl
+        else hd::(remove_redundant_rules tl)
+      | _ -> hd::(remove_redundant_rules tl)
+;;
+
+let rec reduce_rules rules_list =
+  let rec possible_NT rules_list =
+    match rules_list with
+    | [] -> []
+    | hd::tl -> 
+      match hd with (lhs, rhs) ->
+        set_union [lhs] (possible_NT tl)
+  in
+  let valid_NT = possible_NT rules_list in
+  let rec remove_invalid_rules rules_list =
+    match rules_list with
+    | [] -> []
+    | hd::tl ->
+      match hd with (lhs, rhs) ->
+        let rec remove_T l =
+          match l with
+          | [] -> []
+          | hd::tl ->
+            match hd with
+            | N _ -> hd::(remove_T tl)
+            | T _ -> remove_T tl
+        in
+        let cleaned_rhs = remove_T rhs in
+        match set_diff cleaned_rhs valid_NT with
+        | [] -> hd::(remove_invalid_rules tl)
+        | _ -> remove_invalid_rules tl
+  in
+  remove_invalid_rules rules_list
+;;
+
+let find_generating_rules rules_list =
+  let rec first_gen rules_list =
+    match rules_list with
+    | [] -> []
+    | hd::tl -> 
+      match hd with (lhs, rhs) ->
+        let rec check_rhs rhs =
+          match rhs with
+          | [] -> false
+          | hd::tl -> 
+            match hd with
+            | T _ -> true
+            | N _ -> check_rhs tl
+        in 
+        if check_rhs rhs = true then 
+
+
 let print_bool bl =
   match bl with
   | true -> print_endline "true"
@@ -132,4 +258,18 @@ let () = print_bool (equal_sets (while_away ((+) 3) ((>) 10) 0) [0;3;6;9])
 
 let () = print_bool (equal_sets (rle_decode [2,0; 1,6]) [0;0;6])
 let () = print_bool (equal_sets (rle_decode [3,"w"; 1,"x"; 0,"y"; 2,"z"]) ["w"; "w"; "w"; "x"; "z"; "z"])
+
+let () = print_bool (all_terminal [T"0"; T"3"; N"5"])
+let () = print_bool (all_terminal [T"0"; T"3"])
+let () = print_bool (all_terminal [])
+let () = print_bool (all_terminal [N"56"])
+
+let () = print_bool (equal_sets (remove_empty_rhs [N"0", [T"0"]; N"1", []]) [N"0", [T"0"]])
+let () = print_bool (equal_sets (remove_empty_rhs []) [])
+let () = print_bool (equal_sets (remove_empty_rhs [N"1", []; N"0", [T"0"]]) [N"0", [T"0"]])
+
+let () = print_bool (equal_sets (remove_redundant_rules [N"1", []; N"0", [T"0"]; N"1", [N"1"]]) [N"1", []; N"0", [T"0"]])
+
 *)
+
+let () = print_bool (equal_sets (reduce_rules [N"A", [N"Q"]; N"A", [N"B"]; N"A", [N"B"; N"W"]; N"B", [N"C"]; N"C", [T"c"]]) [N"A", [N"B"]; N"B", [N"C"]; N"C", [T"c"]])
